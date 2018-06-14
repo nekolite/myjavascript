590097005825e71d875d6a41bfab33fce3ad5b5c
Nekolite <35808376+nekolite@users.noreply.github.com>
2018-06-14 20:36:03 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+	$(".sidenav").sidenav();
+});
+
+$(document).ready(function () {
+	$(".slider").slider({
+		indicators: false,
+	});
+});
+
+$(document).ready(function () {
+	$(".parallax").parallax();
+});
+
+$(document).ready(function () {
+	$(".materialboxed").materialbox();
+});
+
+$(document).ready(function () {
+	$(".scrollspy").scrollSpy();
+});
